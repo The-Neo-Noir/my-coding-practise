71c616e29d12772272fa5c3ac9fad84588a7a018
Abhiram Mishra <The-Neo-Noir@users.noreply.github.com>
2019-05-02 17:43:34 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,39 @@
+package com.aneonoir.sdalgo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * source:
+ *
+ * tag: math,bitwise
+ *
+ * task:
+ * TODO: can you do it without a string reverse method ?
+ */
+
+public class bitwise {
+
+
+
+    @Test
+    public void test() {
+        assertEquals("101", decimalToBinary(5));
+        assertEquals("100", decimalToBinary(4));
+        assertEquals("10",  decimalToBinary(2));
+        assertEquals("1",   decimalToBinary(1));
+
+    }
+
+    private String decimalToBinary(int i) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while(i >0){
+            int i1 = i % 2;
+            stringBuilder.append(i1 + "");
+            i=i/2;
+
+        }
+        return stringBuilder.reverse().toString();
+    }
+}
