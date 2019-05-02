4e1e8fa1c55f7f157aee2fd670e6d650b3eaeae5
Abhiram Mishra <The-Neo-Noir@users.noreply.github.com>
2019-05-02 16:00:07 +0530

    Add files via upload

---
+++ CheckIfAllBitsOfANumberAreSet.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class CheckIfAllBitsOfANumberAreSet {
+
+    @Test
+    public static void main(String[] args) {
+        int input=7;
+
+        assertTrue( checkIfSet(7));
+        assertFalse(checkIfSet(14));
+        assertFalse(checkIfSet(0));
+
+
+    }
+
+    static  boolean checkIfSet(int input ) {
+       return  (input & (input+1))==0;
+    }
+}
