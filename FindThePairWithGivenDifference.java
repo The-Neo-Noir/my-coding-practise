6b21a8e2f13f0636aa3947c4a800e8a92cd30475
Abhiram Mishra <The-Neo-Noir@users.noreply.github.com>
2019-05-02 15:11:28 +0530

    Add files via upload

---
+++ FindThePairWithGivenDifference.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * Source Link: https://www.geeksforgeeks.org/find-a-pair-with-the-given-difference/
+ *
+ * Stuggle:No
+ *
+ * Tags: array,hash
+ *
+ */
+
+public class FindThePairWithGivenDifference {
+    public static void main(String[] args) {
+
+       int arr[] = {5, 20, 3, 2, 50, 80};
+        int differenceRequired = 78;
+
+        findThePairWithGivenDifference(arr, differenceRequired);
+    }
+
+    // A bruteforce method
+    private static void findThePairWithGivenDifference(int[] arr, int differenceRequired) {
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int currentNumber = arr[i];
+            for (int j = i+1; j < arr.length; j++) {
+                if(Math.abs(currentNumber-arr[j])==differenceRequired){
+                    System.out.println("Match found : " + currentNumber + " " + arr[j]);
+                }
+            }
+        }
+    }
+
+}
