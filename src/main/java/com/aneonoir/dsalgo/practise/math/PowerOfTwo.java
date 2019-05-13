e2ad2bb33248f005127604659b6cc44681a3470b
Abhiram Mishra <abhiram.mishra@wipro.com>
2019-05-13 16:46:31 +0530

    String code

--- src/main/java/com/aneonoir/dsalgo/practise/math/PowerOfTwo.java
+++ src/main/java/com/aneonoir/dsalgo/practise/math/PowerOfTwo.java
@@ -5,6 +5,11 @@ package com.aneonoir.dsalgo.practise.math;
  * link: https://leetcode.com/problems/power-of-two/
  *
  *
+ * struggle: numbers negative or 0 should be checked.
  */
 public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        else return (n& (n-1) )==0;
+    }
 }
