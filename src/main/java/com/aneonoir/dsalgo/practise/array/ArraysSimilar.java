1cee635659cb80174f818c95cbf09d1d6df54f36
Abhiram Mishra <abhiram.mishra@wipro.com>
2019-05-15 17:20:47 +0530

    Array code from leet code

---
+++ src/main/java/com/aneonoir/dsalgo/practise/array/ArraysSimilar.java
@@ -0,0 +1,63 @@
+package com.aneonoir.dsalgo.practise.array;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * link: https://app.codesignal.com/arcade/intro/level-4/xYXfzQmnhBvEKJwXP
+ *
+ * Struggle: when the item is contains then only you should retrieve
+ *
+ * TODO: Did pass, but the code can be optimized
+ */
+public class ArraysSimilar {
+
+    @Test
+    public void test() {
+        boolean b = areSimilar(new int[]{1, 2, 1, 2, 1, 2}, new int[]{1, 1, 1, 2, 2, 2});
+        assertTrue(b);
+
+    }
+
+    boolean areSimilar(int[] a, int[] b) {
+        Map<Integer,Integer> setA= new HashMap<>();
+        Map<Integer,Integer> setB= new HashMap<>();
+        boolean result=false;
+        // All the elements and the element count must be same
+        //
+        //
+        int count=0;
+        for(int i=0;i<a.length;i++){
+            if(setA.containsKey(a[i])){
+                setA.put(a[i],setA.get(a[i])+1);
+
+            }else{
+                setA.put(a[i],0);
+            }
+            if(setB.containsKey(b[i])){
+                setB.put(b[i],setB.get(b[i])+1);
+            }else{
+                setB.put(b[i],0);
+
+            }
+            // positional matches
+            if(a[i]==b[i]){
+                count++;
+            }
+        }
+
+        if((count == a.length-2 && setA.equals(setB))){
+            result=true;
+        }else{
+            result=false;
+        }
+        return result;
+    }
+
+
+}
