9383694f60980789e09559eafdc44882ef9ea0b3
aneonoir <aneonoir@gmail.com>
2019-11-10 02:11:46 +0530

    somemore code

---
+++ src/main/java/com/aneonoir/dsalgo/practise/linkedlist/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,13 @@
+package com.aneonoir.dsalgo.practise.linkedlist;
+
+/**
+ * link: https://practice.geeksforgeeks.org/problems/intersection-of-two-linked-list/1/?ref=self
+ *
+ * approach: a pretty bruteforce once
+ * TODO : is ther a better apporach exits. ??
+ *
+ * tags: interview, list, intersection
+ */
+public class IntersectionofTwoLinkedLists {
+
+}
