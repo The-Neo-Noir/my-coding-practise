6516cd30da9d5e576c63e4e3a4a5e632bf6e313a
Abhiram Mishra <abhiram.mishra@wipro.com>
2019-11-16 20:07:47 +0530

    Tree Level order from leetcode

--- src/main/java/com/aneonoir/dsalgo/practise/linkedlist/RemoveKFromList.java
+++ src/main/java/com/aneonoir/dsalgo/practise/linkedlist/RemoveKFromList.java
@@ -1,36 +1,42 @@
-//package com.aneonoir.dsalgo.practise.linkedlist;
-//
-///**
-// * link: https://app.codesignal.com/interview-practice/task/gX7NXPBrYThXZuanm
-// * <p>
-// * solution: TODO:  a good bit of struggle I faced. must redo
-// */
-//public class RemoveKFromList {
-//
-//    ListNode<Integer> removeKFromList(ListNode<Integer> l, int k) {
-//
-//        ListNode<Integer> pNode = l, cNode = l;
-//        int counter = 0;
-//        boolean flag = false;
-//        while (cNode != null) {
-//
-//            if (cNode.value == k) {
-//                pNode.next = cNode.next;
-//                if (counter == 0) {
-//                    flag = true;
-//                    l = cNode.next;
-//                }
-//            }
-//            pNode = cNode;
-//            cNode = cNode.next;
-//            counter++;
-//            if (flag) {
-//                counter = 0;
-//                flag = false;
-//            }
-//
-//        }
-//        return l;
-//    }
-//
-//}
+package com.aneonoir.dsalgo.practise.linkedlist;
+
+/**
+ * link: https://app.codesignal.com/interview-practice/task/gX7NXPBrYThXZuanm
+ * <p>
+ * solution: TODO: can you do it recursively
+ * <p>
+ * tags: list,linkelist,remove 'k' element from list, interview question
+ */
+public class RemoveKFromList {
+
+    ListNode<Integer> removeKFromList(ListNode<Integer> l, int k) {
+
+        ListNode temp = l, head = l, prev = l;
+
+        while (temp != null) {
+            if (temp.value.equals(k)) {
+                if (head == temp) {
+                    head = temp.next;
+                    prev = temp.next;
+                } else {
+                    prev.next = temp.next;
+                }
+            } else {
+                prev = temp;
+            }
+            temp = temp.next;
+        }
+        return head;
+
+    }
+
+    private class ListNode<T> {
+        T value;
+        ListNode<T> next;
+
+        ListNode(T x) {
+            value = x;
+        }
+    }
+
+}
