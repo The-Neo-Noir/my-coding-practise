ce4f36a2d212b75d1a25aae6a8d9c314920956e9
Abhiram Mishra <abhiram.mishra@wipro.com>
2019-11-18 22:25:35 +0530

    some more code

---
+++ src/main/java/com/aneonoir/dsalgo/practise/bitwise/NumberOf1Bits.java
@@ -0,0 +1,24 @@
+package com.aneonoir.dsalgo.practise.bitwise;
+
+/**
+ * link: https://leetcode.com/problems/number-of-1-bits/
+ * <p>
+ * approach: right shift and & operator.
+ * <p>
+ * tags: right shift , bitwise, & operator ,
+ * <p>
+ * learnings: if ((n & 1) == 1) { is correct but if(n&1==1) is compilation error
+ */
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        int i = 32;
+        while (i-- > 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+}
