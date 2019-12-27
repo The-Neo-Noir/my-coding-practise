bcecbc49ac390e7762d5cc8a3147121a8aa8dc25
Abhiram Mishra <abhiram.mishra@wipro.com>
2019-12-27 17:33:46 +0530

    Old code revisited and fixed this time

--- src/main/java/com/aneonoir/dsalgo/practise/tree/SumRootToLeafNumbers.java
+++ src/main/java/com/aneonoir/dsalgo/practise/tree/SumRootToLeafNumbers.java
@@ -2,6 +2,14 @@ package com.aneonoir.dsalgo.practise.tree;
 
 /**
  * link: https://leetcode.com/problems/sum-root-to-leaf-numbers/
+ *
+ * appraoch: a recursive approach is much more intuitive
+ *
+ * tag: interview,
+ *
+ * TODO: iterative appraoch, can you do it mathematically ( not using a string )
+ *
+ *
  */
 public class SumRootToLeafNumbers {
     public static void main(String[] args) {
@@ -10,17 +18,28 @@ public class SumRootToLeafNumbers {
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         SumRootToLeafNumbers sumRootToLeafNumbers = new SumRootToLeafNumbers();
-        String i = sumRootToLeafNumbers.sumNumbers(0, root);
-        System.out.println(i);
+        //  String i = sumRootToLeafNumbers.sumNumbers(0, root);
+        //  System.out.println(i);
     }
 
-    public String sumNumbers(int val, TreeNode root) {
-
-        if (root == null) {
-            return "";
+    public int sumNumbers(TreeNode root) {
+        if (root != null) {
+            return sumNumbers(root, "");
         } else {
-            return root.val + "" + "->" + sumNumbers(root.val, root.left) +
-                    root.val + "R>" + sumNumbers(root.val, root.right);
+            return 0;
+        }
+    }
+
+    public int sumNumbers(TreeNode root, String in) {
+        if (root != null) {
+            String val = in + "" + root.val;
+            if (root.left == null && root.right == null) {
+                return Integer.parseInt(val);
+            }
+            return sumNumbers(root.left, val) + sumNumbers(root.right, val);
+
+        } else {
+            return 0;
         }
     }
 }
