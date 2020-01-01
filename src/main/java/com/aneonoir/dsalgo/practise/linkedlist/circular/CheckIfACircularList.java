d6e51bcbcaf528bcb555b56bafe8150f0eecef41
Abhiram Mishra <abhiram.mishra@wipro.com>
2020-01-01 15:27:33 +0530

    circular linked list super easy question

---
+++ src/main/java/com/aneonoir/dsalgo/practise/linkedlist/circular/CheckIfACircularList.java
@@ -0,0 +1,25 @@
+package com.aneonoir.dsalgo.practise.linkedlist.circular;
+
+import com.aneonoir.dsalgo.practise.linkedlist.Node;
+
+/**
+ * 01/01/20 3:26 PM
+ * link: https://practice.geeksforgeeks.org/problems/circular-linked-list/1
+
+ * TODO: can you do it recursively ?
+ */
+public class CheckIfACircularList {
+    public boolean isCircular(Node node) {
+        if (node == null) return true;
+        Node temp = node;
+        boolean result = false;
+        while (node != null) {
+            if (node.next == temp) {
+                result = true;
+                break;
+            }
+            node = node.next;
+        }
+        return result;
+    }
+}
