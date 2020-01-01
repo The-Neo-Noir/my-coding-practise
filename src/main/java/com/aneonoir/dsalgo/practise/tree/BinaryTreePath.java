d7d7b8e475a3cff864b97783549eff6bf4e60645
ANeoNoir <aneonoir@gmail.com>
2020-01-02 02:54:09 +0530

    A pending code which I was not able to crack using recursion.

--- src/main/java/com/aneonoir/dsalgo/practise/tree/BinaryTreePath.java
+++ src/main/java/com/aneonoir/dsalgo/practise/tree/BinaryTreePath.java
@@ -1,9 +1,19 @@
 package com.aneonoir.dsalgo.practise.tree;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * a Major struggle
+ * link: https://leetcode.com/problems/binary-tree-paths/
+ *
+ * approach: recursive, and solved it after few exploration.
+ *
+ * TODO: can you do it iteratively ?
+ *
+ * Runtime Runtime: 1 ms, faster than 100.00% of Java online submissions for Binary Tree Paths.
+ * 02/01/20 2:53 AM
+ *
+ *
  */
 public class BinaryTreePath {
 
@@ -20,25 +30,52 @@ public class BinaryTreePath {
     }
 
     public List<String> binaryTreePaths(TreeNode root) {
-        String strings = binaryTreePath(root, new StringBuffer());
-        System.out.println(strings);
-        return null;
+        List<String> paths= new LinkedList();
+        if(root!=null){
+            binaryTreePaths(root,paths,"");
+        }
+        return paths;
     }
-
-
-    private String binaryTreePath(TreeNode root, StringBuffer sbr) {
-        if (root.left == null && root.right == null) {
-            System.out.println(root.val + "");
-            return root.val + "";
-        } else {
-            String rootS = root.val + "->";
-            String rootR = root.val + "->";
-            if (root.left != null)
-                rootS += binaryTreePath(root.left, sbr);
-            if (root.right != null) {
-                rootR += binaryTreePath(root.right, sbr);
+    private void binaryTreePaths(TreeNode root, List<String> paths,String pre ){
+        if(root!=null){
+            String st=pre+root.val;
+            if(root.left==null && root.right==null){
+                paths.add(st);
             }
-            return rootS + "SSS" + rootR;
+            st=st+"->";
+            binaryTreePaths(root.left,paths,st);
+            binaryTreePaths(root.right,paths,st);
         }
     }
 }
+
+/**
+ *  02/01/20 2:48 AM All these did not work
+ * @param root
+ * @return
+
+public List<String> binaryTreePaths(TreeNode root) {
+    String strings = binaryTreePath(root, new StringBuffer());
+    System.out.println(strings);
+    return null;
+}
+
+
+private String binaryTreePath(TreeNode root, StringBuffer sbr) {
+    if (root.left == null && root.right == null) {
+        System.out.println(root.val + "");
+        return root.val + "";
+    } else {
+        String rootS = root.val + "->";
+        String rootR = root.val + "->";
+        if (root.left != null)
+            rootS += binaryTreePath(root.left, sbr);
+        if (root.right != null) {
+            rootR += binaryTreePath(root.right, sbr);
+        }
+        return rootS + "SSS" + rootR;
+    }
+}
+
+}
+*/
