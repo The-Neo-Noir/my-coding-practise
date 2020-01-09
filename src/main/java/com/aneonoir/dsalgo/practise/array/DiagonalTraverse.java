ad9913f9cb333b0ca1f73b90db385e37959b6cdb
ANeoNoir <aneonoir@gmail.com>
2020-01-10 02:19:09 +0530

    diagonal traversal and triangle

---
+++ src/main/java/com/aneonoir/dsalgo/practise/array/DiagonalTraverse.java
@@ -0,0 +1,90 @@
+package com.aneonoir.dsalgo.practise.array;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * I did not do it mindfully, I have to do it again. Did a lot of debugging, in the ide,
+ *
+ * The main struggle, was in finding the duplicate, then I realised, I don't need to check for duplicate.
+ *
+ * TODO: must do agian.,  can you do it in iterattion and and recursioon ?
+ *
+ *
+ * tag: interview, matrix, traversal , struggle
+ */
+public class DiagonalTraverse {
+
+    public static void main(String[] args) {
+        DiagonalTraverse diagonalTraverse = new DiagonalTraverse();
+        int[] diagonalOrder = diagonalTraverse.findDiagonalOrder(new int[][]{{1, 2, 3}, {3, 4, 5}, {6, 7, 8}});
+
+
+    }
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if(matrix.length>0){
+            Queue<Cell> que = new LinkedList<>();
+            que.add(new Cell(0, 0));
+            List<Integer> result = new LinkedList<>();
+            int counter = 0;
+            while (!que.isEmpty()) {
+                List<Cell> cells = new LinkedList<>();
+                while (!que.isEmpty()) {
+                    cells.add(que.remove());
+                }
+                for (int i = 0; i < cells.size(); i++) {
+                    Cell s = cells.get(i);
+                    if (s.row <= matrix.length - 1 && s.col + 1 <= matrix[s.row].length-1) {
+                        Cell e = new Cell(s.row, s.col + 1);
+                        que.add(e);
+
+                    }
+                    if (i == cells.size() - 1) {
+                        if (s.row + 1 <= matrix.length - 1 && s.col <= matrix[s.row + 1].length-1) {
+                            que.add(new Cell(s.row + 1, s.col));
+                        }
+                    }
+                }
+                for (int i = 0; i < cells.size(); i++) {
+                    if (counter % 2 == 0) {
+                        result.add(matrix[cells.get(cells.size() - 1 - i).row][cells.get(cells.size() - 1 - i).col]);
+                    } else {
+                        result.add(matrix[cells.get(i).row][cells.get(i).col]);
+                    }
+                }
+                counter++;
+            }
+            int resultFinal[] = new int[result.size()];
+            int i = 0;
+            for (Integer in : result) {
+                resultFinal[i++] = in;
+            }
+            return resultFinal;
+        }else{
+            return new int[0];
+        }
+
+    }
+
+    class Cell {
+        int row, col;
+
+        Cell(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            } else {
+                if (obj instanceof Cell) {
+                    Cell cell = (Cell) obj;
+                    return this.row - cell.row == 0 ? this.col - cell.col == 0 ? true : false : false;
+                } else return false;
+            }
+        }
+
+    }
+}
--- src/main/java/com/aneonoir/dsalgo/practise/array/Triangle.java
+++ src/main/java/com/aneonoir/dsalgo/practise/array/Triangle.java
@@ -1,24 +1,39 @@
 package com.aneonoir.dsalgo.practise.array;
 
-import java.util.Collections;
 import java.util.List;
 
+/**
+ * On the second attempt , I did the following,.
+ * start from the bottom, with the minium value, once found go up in the level, with the adjecent value, but I believe it fails
+ * > Yes off course it would fail,
+ * > I need to count all the values, along the way and output the lowest. Its an optimization problem, thus dynamic programming. Can you solve it.
+ * > Finally solved.
+ *
+ *
+ * approach: working, start from the second last row, count the min by calculating last row,
+ *
+ * tag: dynamic programming, interview, struggle,
+ *
+ * TODO: can you do it using memoisation
+ */
 public class Triangle {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int sum = 0;
-        for (List<Integer> list : triangle) {
-//            0->0
-//            1->01;
-//            2->01;0
-//            2->12;1
-//            3->01 0;
-//            3->012 1;
-//            3->123 2
-
-
-            Collections.sort(list);
-            sum += list.get(0);
+        if (triangle != null && triangle.size() > 0) {
+            for (int i = triangle.size() - 2; i >= 0; i--) {
+                manipulte(triangle, i, i + 1);
+            }
+            return triangle.get(0).get(0);
         }
-        return sum;
+        return 0;
+
+    }
+
+    private void manipulte(List<List<Integer>> triangle, int manipulateLevel, int referenceLevel) {
+        List<Integer> manipulateLevelList = triangle.get(manipulateLevel);
+        List<Integer> referenceLevelList = triangle.get(referenceLevel);
+        for (int i = 0; i < referenceLevelList.size() - 1; i++) {
+            manipulateLevelList.set(i, Math.min(referenceLevelList.get(i) + manipulateLevelList.get(i), manipulateLevelList.get(i) + referenceLevelList.get(i + 1)));
+        }
+
     }
 }
